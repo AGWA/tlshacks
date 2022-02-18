beb0b33715668e78f648ac17f7ab6e6f56710b70
Andrew Ayer <agwa@andrewayer.name>
2022-02-17 21:40:33 -0500

    Add parsers for support groups and EC point formats extensions

--- extension.go
+++ extension.go
@@ -102,8 +102,67 @@ func ParseALPNData(raw []byte) ExtensionData {
 	return alpnData
 }
 
+// RFC 8422
+// TODO: provide info about groups beyond just the code (name, grease status, etc.)
+type SupportedGroupsData struct {
+	Raw    []byte   `json:"raw"`
+	Valid  bool     `json:"valid"`
+	Groups []uint16 `json:"groups"`
+}
+
+func ParseSupportedGroupsData(rawData []byte) ExtensionData {
+	parsedData := &SupportedGroupsData{Raw: rawData, Groups: []uint16{}}
+	data := cryptobyte.String(rawData)
+	var groupList cryptobyte.String
+	if !data.ReadUint16LengthPrefixed(&groupList) || groupList.Empty() {
+		return parsedData
+	}
+	for !groupList.Empty() {
+		var groupCode uint16
+		if !groupList.ReadUint16(&groupCode) {
+			return parsedData
+		}
+		parsedData.Groups = append(parsedData.Groups, groupCode)
+	}
+	if !data.Empty() {
+		return parsedData
+	}
+	parsedData.Valid = true
+	return parsedData
+}
+
+// RFC 8422
+type ECPointFormatsData struct {
+	Raw     []byte   `json:"raw"`
+	Valid   bool     `json:"valid"`
+	Formats []uint16 `json:"formats"`
+}
+
+func ParseECPointFormatsData(rawData []byte) ExtensionData {
+	parsedData := &ECPointFormatsData{Raw: rawData, Formats: []uint16{}}
+	data := cryptobyte.String(rawData)
+	var list cryptobyte.String
+	if !data.ReadUint8LengthPrefixed(&list) || list.Empty() {
+		return parsedData
+	}
+	for !list.Empty() {
+		var code uint8
+		if !list.ReadUint8(&code) {
+			return parsedData
+		}
+		parsedData.Formats = append(parsedData.Formats, uint16(code))
+	}
+	if !data.Empty() {
+		return parsedData
+	}
+	parsedData.Valid = true
+	return parsedData
+}
+
 var extensionParsers = map[uint16]func([]byte) ExtensionData{
 	0:  ParseServerNameData,
+	10: ParseSupportedGroupsData,
+	11: ParseECPointFormatsData,
 	16: ParseALPNData,
 	18: ParseEmptyExtensionData,
 	22: ParseEmptyExtensionData,
